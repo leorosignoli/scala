173ddd75ae5c104c8876008bc9cb6c6da5441240
leorosignoli <55900426+leorosignoli@users.noreply.github.com>
2023-04-05 15:09:28 -0300

    Colored Boxes challenge

--- LinkedChallenges/build.sbt
+++ LinkedChallenges/build.sbt
@@ -1,10 +1,15 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.10"
+ThisBuild / scalaVersion := "3.2.2"
 
 lazy val root = (project in file("."))
   .settings(
     name := "LinkedChallenges",
     idePackagePrefix := Some("edu.linkedin.challenges"),
-    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % "test"
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.15" % "test",
+    libraryDependencies ++= Seq(
+      "org.creativescala" %% "doodle" % "0.18.0",
+      "org.scalameta" %% "munit" % "0.7.29" % Test
+    )
+
   )
---
+++ LinkedChallenges/src/main/scala/ColoredBoxes.scala
@@ -0,0 +1,28 @@
+package edu.linkedin.challenges
+
+import cats.effect.unsafe.implicits.global
+import doodle.image.Image
+import doodle.core.Color
+import doodle.image.syntax.all.*
+import doodle.java2d.*
+
+import scala.annotation.tailrec
+
+object ColoredBlocks {
+
+  private val block = Image.square(40).fillColor(Color.blue)
+  private var stackOfBlocks: Image = Image.empty
+
+  @tailrec
+  private def stack(count: Int): Image =
+    if (count == 0) stackOfBlocks
+    else
+      stackOfBlocks = stackOfBlocks above(block)
+      stack(count - 1)
+
+
+
+  @main def drawBlocks(): Unit =
+    stack(5).draw()
+}
+
