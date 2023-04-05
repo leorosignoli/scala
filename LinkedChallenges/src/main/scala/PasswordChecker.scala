d12ada5103366ec9966d5faeb2ed0219b0ec2928
leorosignoli <55900426+leorosignoli@users.noreply.github.com>
2023-04-04 21:59:57 -0300

    Update PasswordChecker.scala

--- LinkedChallenges/src/main/scala/PasswordChecker.scala
+++ LinkedChallenges/src/main/scala/PasswordChecker.scala
@@ -2,13 +2,14 @@ package edu.linkedin.challenges
 
 import PasswordChecker.MINIMAL_PASSWORD_SIZE
 
-object PasswordChecker {
+class PasswordChecker(validator : StringValidator) {
 
-  val MINIMAL_PASSWORD_SIZE = 8
+
   def isValidPassword(password: String): List[String] = {
-    StringValidator
-      .validateThat(password)
+    validator
+       .validateThat(password)
       .hasEightChars()
+      .hasUppercaseLetters()
   }
 
 }
@@ -16,14 +17,21 @@ object PasswordChecker {
 
 
 
-object StringValidator {
-
-  def validateThat(string :String)
-
-  def hasEightChars(string: String): StringValidator = {
-    if (stringToValidate.length.>=(MINIMAL_PASSWORD_SIZE))
-
+class StringValidator(str: String, errorList: List[String] = List()) {
+  val MINIMAL_PASSWORD_SIZE = 8
+  def validateThat(string :String): StringValidator ={
+  return new StringValidator(string)
   }
+
+  def hasEightChars(): StringValidator = {
+    if (str.length.>=(MINIMAL_PASSWORD_SIZE)) {
+      errorList.appended("Password does not have %s chars".formatted(MINIMAL_PASSWORD_SIZE))
+    }
+    this
+  }
+
+  def hasUppercaseLetters()
+
 }
 
 
